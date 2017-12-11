ebbcab5f3972ca46a724655524bdd3ae62591aec
lriverawong <lriverawong@outlook.com>
2017-12-11 00:53:16 -0500

    finished section 08

--- 02-javascript-fundamentals/08-type-conversions/app.js
+++ 02-javascript-fundamentals/08-type-conversions/app.js
@@ -1,36 +1,45 @@
-// data-types
+// type conversions
 
- // primitive types
- // string
- const name = 'john';
- console.log(typeof name);
-// number
-const aNum = 45;
-console.log(typeof aNum);
-// boolean
-const hasKids = true;
-console.log(typeof hasKids);
-// null
-const car = null;
-console.log(typeof car);
-// undefined
-let test = undefined;
-console.log(typeof test);
-// symbol
-const sym = Symbol();
-console.log(typeof sym);
+let val;
 
-/* 
-  Reference Types
+// number to string
+val = 5;
+val = String(val);
+val = String(4+4);
+// Boolean to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to stirng
+val = String([1,2,3,4])
+// toString() method
+val = (5).toString();
+val = (true).toString();
+
+// string to number
+// val = '5'
+// val = Number(val);
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number('hello');
+
+val = parseInt('100'); // number -> 100
+val = parseInt('100.30'); // number -> 100
+val = parseFloat('100.30'); // number -> 100.30
+
+// console output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed(2))
+
+/*
+  Type coersion
 */
-const hobbies = ['bjj', 'biking'];
-const address = {
-  province: 'ON',
-  city: 'Kingston'
-}
-const dateToday = new Date();
-
-console.log(typeof hobbies);
-console.log(typeof address);
-console.log(typeof dateToday);
-
+// const val1 = 5; // results in (number) 11
+const val1 = String(5); // results in (string) 56
+const val2 = 6;
+const sum = val1 + val2;
+console.log(sum);
+console.log(typeof sum);
